6212cabc65e40bf73ce3b7f8698a1d5cd294da3a
bencejenei <bencejenei94@gmail.com>
2024-02-03 19:41:15 +0900

    Create "player stats" raw datafile

--- virtualcourt/data/datafiles.py
+++ virtualcourt/data/datafiles.py
@@ -3,11 +3,15 @@ import virtualcourt.data.raw as rawfolder
 
 
 def main():
-    print(player_attributes())
+    pass
 
 
 def player_attributes():
     return f"{os.path.dirname(__file__)}/raw/player_attributes.json"
+
+
+def player_stats():
+    return f"{os.path.dirname(__file__)}/raw/player_stats.json"
 
 
 if __name__ == "__main__":
